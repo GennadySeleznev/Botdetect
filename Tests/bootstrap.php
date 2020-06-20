8e25d3dd886048eef6540a774e649344f657e52d
gennnadyseleznev <predradinkic@gmail.com>
2020-06-20 17:28:45 +1000

    newbranch_test

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -7,6 +7,7 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ * This is my selfreliance work about "making new git test"
  */
 
 $file = __DIR__.'/../vendor/autoload.php';
